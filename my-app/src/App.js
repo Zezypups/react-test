3c1928ce08e6339c56a2330f68e2dd0d5842a5d5
Zezypups <109433579+Zezypups@users.noreply.github.com>
2023-02-18 19:47:29 +0400

    "Profile" component
    My first react component!

---
+++ my-app/src/App.js
@@ -0,0 +1,25 @@
+
+import './App.css';
+
+function Profile() {
+  return (
+    <div className="Profile">
+      <header className="Profile-header">
+        <img src="Profile.jpg" className="Profile-logo" alt="logo" />
+        <p>
+          Hello, I am ZezyPups !
+        </p>
+        <a
+          className="Profile-link"
+          href="https://github.com/Zezypups"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Here is my Github
+        </a>
+      </header>
+    </div>
+  );
+}
+
+export default Profile;
